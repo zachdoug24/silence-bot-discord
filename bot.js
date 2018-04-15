f7ae4e76cb4886bf92635110a8b881d0e4326946
zachdoug24 <30303429+zachdoug24@users.noreply.github.com>
2018-04-15 10:27:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,26 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on("message", message => {
-  const prefixes = ['.'];
-  let prefix = false;
-  for(const thisPrefix of prefixes) {
-    if(message.content.startsWith(thisPrefix)) prefix = thisPrefix;
-  }
-  if(!prefix) return;
-    const user = message.mentions.users.first();
-    const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-    if (!amount) return message.reply('Must specify an amount to delete!');
-    if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-    message.channel.fetchMessages({
-    limit: amount,
-        }).then((messages) => {
-    if (user) {
-    const filterBy = user ? user.id : Client.user.id;
-     messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-    }
- message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
