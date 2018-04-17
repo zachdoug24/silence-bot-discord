50ccbaaded5290192bbdcdd13333975101de1a68
zachdoug24 <30303429+zachdoug24@users.noreply.github.com>
2018-04-17 17:02:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('guildMemberAdd', member => {
   // Send the message, mentioning the member
   
   const embed = {
-  "description": "Please be sure you check out the <#434919397086330883> channel for everything you need to know about your stay here.",
+  "description": "Dear " + member.displayName + ", lease be sure you check out the <#434919397086330883> channel for everything you need to know about your stay here.",
   "color": 5296491,
   "footer": {
     "icon_url": "https://cdn.discordapp.com/avatars/161331492730044416/58457f95610d97c47c380842b2c00fc4.png",
