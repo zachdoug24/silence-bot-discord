3f6174727383b358a435d1a3873dccd2a0412f6a
zachdoug24 <30303429+zachdoug24@users.noreply.github.com>
2018-05-11 14:06:37 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,8 +1,8 @@
 exports.run = (client, message, args) => {
    message.delete();
    const embed = {
-   "description": """ + message.author.username + " says: " +args.join(" ")",
-   "color": 0x6a6a6a,
+   "description": args.join(" "),
+   "color": 0x6a6a6a
    };
    message.channel.send({embed})
 }
