5c7f875c1c4911ceccf2e4535d0ea0bf6de9136f
zachdoug24 <30303429+zachdoug24@users.noreply.github.com>
2018-05-11 13:54:11 -0500

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+   message.delete()
+   const embed = new Discord.RichEmbed()
+   .setColor(0x6a6a6a)
+   .setDescription(`"" + message.author.username + " says: " +args.join(" "))
+   message.channel.send({embed})
+}
