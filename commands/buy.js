b9646a3957045a625fffd6cc8b6e585e870f6919
zachdoug24 <30303429+zachdoug24@users.noreply.github.com>
2018-05-11 14:43:06 -0500

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
    message.delete();
    const embed = {
    "description": message.author.displayName + " is wishing to buy " + args.join(" "),
-   "color": 0x00ff80
+   "color": 0x00ff80,
    "author": {
     "name": message.author.displayName + " is buying something.",
     "icon_url": message.author.displayAvatarURL
