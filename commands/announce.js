cebf1ba15a5b09fa48b6db6ec49c4f4ddf3119f3
Silence <zachdoug24@gmail.com>
2018-05-13 15:56:14 -0500

    Revised code.

--- commands/announce.js
+++ commands/announce.js
@@ -10,19 +10,16 @@ exports.run = (client, message, args) => {
        }
    };
    message.channel.send({embed});
-   
+},
+{
    const channel = member.guild.channels.find('name', 'mod-logs');
     if (!channel) return;
     const embed = {
-    "description": "Welcome " + member + "!\n\nPlease be sure you check out the <#434919397086330883> channel for everything you need to know about your stay here.",
-    "color": 5296491,
-    "footer": {
-      "icon_url": "https://cdn.discordapp.com/avatars/161331492730044416/58457f95610d97c47c380842b2c00fc4.png",
-      "text": "Created with Silence"
-    },
+    "description": "Created an announcement.",
+    "color": 4028623,
     "author": {
-      "name": member.displayName + " has joined.",
-      "icon_url": member.user.displayAvatarURL
+        "text": message.author.username,
+        "icon_url": message.author.displayAvatarURL
     }
   };
   
