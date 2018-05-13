dbb46d35d2874049626c35f5e932280110b3c4dd
Silence <zachdoug24@gmail.com>
2018-05-13 12:37:56 -0500

    updated kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,28 +1,32 @@
 exports.run = (client, message, [mention, ...reason]) => {
   const modRole = message.guild.roles.find("name", "Jack");
   if (!modRole)
-    return console.log("The Moderator role does not exist");
+    return console.log("The Jack role does not exist.");
 
   if (!message.member.roles.has(modRole.id))
     return message.reply("You don't have permission to use this command.");
 
   if (message.mentions.members.size === 0)
-    return message.reply("Please mention a user to kick");
+    return message.reply("Please mention a user to kick.");
 
   if (!message.guild.me.hasPermission("KICK_MEMBERS"))
     return message.reply("");
 
+  const kickMember = message.mentions.members.first();
+
   const embed = {
-   "color": 14700624,
-   "footer": {
+    "author": {
+      "name": member.displayName + " has been kicked.",
+      "icon_url": member.user.displayAvatarURL
+    },
+    "description": reason,
+    "color": 14700624,
+    "footer": {
     "icon_url": "https://cdn.discordapp.com/avatars/161331492730044416/58457f95610d97c47c380842b2c00fc4.png",
     "text": "Created with Silence"
-  },
-  "author": {
-    "name": member.displayName + " has been kicked.",
-    "icon_url": member.user.displayAvatarURL
   }
 };
+
   kickMember.kick(reason.join(" ")).then(member => {
     message.reply({ embed });
   });
