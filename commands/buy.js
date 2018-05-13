4d9df7a6b25cb3a797c507e5736949a456f5bf4b
Silence <zachdoug24@gmail.com>
2018-05-13 11:05:15 -0500

    edited buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,29 +1,29 @@
-exports.run = (client, message, args) => {
-   let description = args.slice(0)[0].join(" ")
-   let price = args.slice(1)[1].join(" ");
-   message.delete();
-   const embed = {
-  "color": 10081886,
-  "author": {
-    "name": message.author.username,
-    "icon_url": message.author.displayAvatarURL
-  },
-  "fields": [
-    {
-      "name": "Wanting to Purchase",
-      "value": description
+export function run(client, message, args) {
+  let price = args[0];
+  let description = args[1];
+  message.delete();
+  const embed = {
+    "color": 10081886,
+    "author": {
+      "name": message.author.username,
+      "icon_url": message.author.displayAvatarURL
     },
-    {
-      "name": "Purchase Price",
-      "value": price,
-      "inline": true
-    },
-    {
-      "name": "Contact User",
-      "value": message.author.tag,
-      "inline": true
-    }
-  ]
-};
-message.channel.send({embed})
+    "fields": [
+      {
+        "name": "Wanting to Purchase",
+        "value": description
+      },
+      {
+        "name": "Purchase Price",
+        "value": price,
+        "inline": true
+      },
+      {
+        "name": "Contact User",
+        "value": message.author.avatarURL + " " + message.author.tag,
+        "inline": true
+      }
+    ]
+  };
+  message.channel.send({embed});
 }
